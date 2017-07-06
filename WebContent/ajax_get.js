848ba1281c72e9f912f9ff919790c72668434106
tss <tss@11.11.11.28>
2017-07-06 12:28:55 +0800

    Added Ajax get

---
+++ WebContent/ajax_get.js
@@ -0,0 +1,28 @@
+/*
+ * Ajax Get method
+ */
+
+//JSON Format
+var toSer = {  //Data that need to be send to server
+  "venue": venue, 
+  "startDate": newStartdate, 
+  "endDate": newEnddate
+  }
+		  
+$.ajax({
+  type: "GET", //Method Used
+  
+  url:"", //API Path
+  
+  data:toSer, //Data that need to be send to server
+  
+  success:function(data) {
+    alert("Success"); /*
+                       * What to do if it is connected succescfully
+                       * -> data.key (JSON Format)
+                       */
+  },
+  error:function(textStatus, errorThrown) {
+    console.log(textStatus);//Display error in console log
+  }
+});
