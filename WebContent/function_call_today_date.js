c3e7807a55a12faa92123cca06d45588f3c1f95a
tss <diinghao.92@gmail.com>
2017-08-29 17:08:27 +0800

    Change Number in MM to Char Function

--- WebContent/function_call_today_date.js
+++ WebContent/function_call_today_date.js
@@ -11,5 +11,35 @@ function callTodayDate(){
               mm='0'+mm;
           } 
           today = dd+'/'+mm+'/'+yyyy;
-          return today;
+          return today; //can change to return MM
       }
+
+//Change Number in MM to Char
+function changeNumtoCharMonth(mm){
+	if(mm == 1){
+		mm = "January";
+	}else if(mm == 2){
+		mm = "February";
+	}else if(mm == 3){
+		mm = "March";
+	}else if(mm == 4){
+		mm = "April";
+	}else if(mm == 5){
+		mm = "May";
+	}else if(mm == 6){
+		mm = "June";
+	}else if(mm == 7){
+		mm = "July";
+	}else if(mm == 8){
+		mm = "August";
+	}else if(mm == 9){
+		mm = "September";
+	}else if(mm == 10){
+		mm = "October";
+	}else if(mm == 11){
+		mm = "November";
+	}else if(mm == 12){
+		mm = "December";
+	}
+	return mm;
+}
