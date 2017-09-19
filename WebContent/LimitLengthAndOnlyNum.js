f00d4a737a3e5173bbf3ff0149b40b4a4fc071ec
tss <diinghao.92@gmail.com>
2017-09-19 10:10:34 +0800

    Update

--- WebContent/LimitLengthAndOnlyNum.js
+++ WebContent/LimitLengthAndOnlyNum.js
@@ -1,19 +1,23 @@
     function check(e,value){
-    //Check Charater
         var unicode=e.charCode? e.charCode : e.keyCode;
-        if (value.indexOf(".") != -1)if( unicode == 46 )return false;
-        if (unicode!=8)if((unicode<48||unicode>57)&&unicode!=46)return false;
+        if (value.indexOf(".") != -1)
+        	if( unicode == 46 )
+        		return false;
+        
+        if (unicode!=8)
+        	if((unicode<48||unicode>57)&&unicode!=46)
+        		return false;
     }
+    
     function checkLength(){
-    var fieldLength = document.getElementById('txtF').value.length;
-    //Suppose u want 4 number of character
+    var fieldLength = document.getElementById("").value.length;
     if(fieldLength <= 16){
         return true;
     }
     else
     {
-        var str = document.getElementById('txtF').value;
+        var str = document.getElementById("").value;
         str = str.substring(0, str.length - 1);
-        document.getElementById('txtF').value = str;
+        document.getElementById("").value = str;
     }
     }
